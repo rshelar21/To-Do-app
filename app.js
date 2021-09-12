f08a9140eaa2e463f9a9e3966b4ec526ee152cac
rohan <rshelar796@gmail.com>
2021-09-12 18:52:09 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 80;
+const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
